f1e2e2f544fa7f4763559fe14b4493da58a1e981
Heather Mason <heathermason@Heathers-MacBook-Pro.local>
2018-09-29 17:25:52 -0700

    added list

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Grocery Shopper',
-      home: new List()
+      home: new GroceryList()
     );
   }
 }
 
-class List extends StatefulWidget {
+class GroceryList extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _ListState();
 }
 
-class _ListState extends State<List> {
+// class ListDisplay extends StatefulfulWidget {
+//   @override
+//   Widget build (BuildContext context) {
+//     return new Scaffold(
+//       body: new ListView.builder(
+//         itemCount: itemsList.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           return new Text(itemsList[index]);
+//         }
+//       )
+//     );
+//   }
+// }
+class _ListState extends State<GroceryList> {
+  
   String item = '';
-  final _stores = ['Safeway', 'Trader Joe\'s', 'Costco'];
+  List<String> itemsList=[];
+  final _stores = [ 'Safeway', 'Trader Joe\'s', 'Costco'];
   String _store = 'Costco';
+  TextEditingController itemFromStoreController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CHANGE ME'),
+        title: Text('Grocery List'),
         backgroundColor: Colors.blue,
       ),
       body: Container(
@@ -33,6 +50,7 @@ class _ListState extends State<List> {
         child: Column(
           children: <Widget>[
             TextField(
+              controller: itemFromStoreController,
               decoration: InputDecoration(
                 labelText: 'Buy:',
                 hintText: 'What do you need?',
@@ -40,35 +58,52 @@ class _ListState extends State<List> {
                   borderRadius: BorderRadius.circular(5.0)
                 )
               ),
-              onSubmitted: (String string) {
-                setState(() {
-                  item = string;
-                });
-              },
+              // onSubmitted: (String string) {
+              //   setState(() {
+              //     item = string;
+              //   });
+              // },
             ),
             DropdownButton<String>(
               // TODO: Add border to dropdown box?
               items: _stores.map((String value){
                 return DropdownMenuItem<String> (
                   value: value,
-                  child: Text(value)
+                  child: new Text(value)
                 );
               }).toList(),
               value: _store,
-              onChanged: (String value) {
+              onChanged: (value) {
                 _onDropdownChanged(value);
-              },
+              }
             ),
             RaisedButton(
               color: Colors.blueAccent,
               child: Text(
-                'Submit',
+                'Add Item',
                 textScaleFactor: 1.5,
                 color: Colors.white,
               ), 
-              onPressed: () {},
+              onPressed: () {
+                setState((){
+                //   // TODO: add colum to list
+                  item = itemFromStoreController.text;
+                  itemsList.add(item);
+                  itemFromStoreController.clear();
+                });
+              },
             ),
-            Text(item)
+            // Text(itemsList),
+            // _buildGroceryList(itemsList),
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: itemsList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new Text(itemsList[index],
+                  textDirection: TextDirection.ltr,);
+                }
+              )
+            )
           ],
         )
       ),
@@ -80,6 +115,16 @@ class _ListState extends State<List> {
           this._store = value;
         });
   }
+}
+
+Widget _buildGroceryList(itemsList) {
+  return new ListView.builder(
+    itemCount: itemsList.length,
+    padding: EdgeInsets.all(14.0),
+    itemBuilder: (BuildContext _context, int index) {
+      return new Text(itemsList[index]);
+    }
+  );
 }
 
 // DONE: add a textfield to enter a grocery item
